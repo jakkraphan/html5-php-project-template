926f1bdfacfe7ce6be631ec380045fa096758c01
stephan lindauer <stephan.lindauer@gmail.com>
2013-01-17 22:32:11 +0200

    added header meta data + reorganized scripts

--- includes/head.php
+++ includes/head.php
@@ -1,12 +1,52 @@
 <meta charset="utf-8">
 
-<title>Easy HTML5 Template</title>
-<meta name="description" content="punini.de html5 template">
+<title>HTML5 Template</title>
+
 <meta name="author" href="humans.txt" content="stephan lindauer - punini.de">
 
+<meta name="description" content="punini.de html5 template">
+<meta name="keywords" content="punini, blog, code, design, css, typography, jquery, flash, actionscript, photoshop, java, javascript, html, php, programming, design, computer science in media, medieninformatik" />
+
+<!-- allow indexing follow any link -->
+<meta name="robots" content="index,follow" />
+
+<!-- Genormte Geodaten zur Adresse -->
+<meta name="geo.region" content="DE-BE" />
+<meta name="geo.position" content="52.46819;13.42611" />
+<meta name="geo.placename" content="emser str., 12051 berlin-neukoelln" />
+<meta name="ICBM" content="52.46819,13.42611" />
+
+<!-- website uses java-script -->
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+<!-- website uses css -->
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<!-- content in english and german -->
+<meta http-equiv="content-language" content="en,de" />
+
+<!-- disables proxy-caching -->
+<meta http-equiv="pragma" content="no-cache" />
+<!-- disables caching -->
+<meta http-equiv="cache-control" content="no-cache" />
+<!-- instant expiration -->
+<meta http-equiv="Expires" content="0" />
+
+<!-- disables useless toolbars -->
+<!--[if IE]>
+<meta http-equiv="imagetoolbar" content="no" />
+<meta name="slice" scheme="IE" content="off" />
+<![endif]-->
+
+<!-- uncomment when testing for ie7 compatability in higher ie version -->
+<!-- <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" /> -->
+
 <link rel="shortcut icon" href="/favicon.ico">
+<link rel="icon" href="assets/favicon.ico" type="image/x-icon"/>
+
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-<link rel="stylesheet" href="css/styles.css?salt=salty">
+<link rel="image_src" href="img/logo.png" />
 
-<script src="js/modernizr-2.0.6.min.js"></script>
+<!-- links up rss feed -->
+<link rel="alternate" type="application/rss+xml" title="punini.de rss-feed" href="rss.php" />
+
+<link rel="stylesheet" href="css/styles.css?salt=salty">
