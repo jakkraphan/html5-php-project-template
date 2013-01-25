dba0521df2df4c0b42c65bfba713d06d19fc473f
stephan lindauer <stephan.lindauer@gmail.com>
2013-01-25 19:16:07 +0200

    jquery update

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,7 +1,7 @@
-<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
 
 <!-- if jquery can not be loaded from the cdn load the local .js file -->
-<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery-1.8.3.min.js"%3E%3C/script%3E'))</script>
+<script>!window.jQuery && document.write(unescape('%3Cscript src="js/jquery.min.js"%3E%3C/script%3E'))</script>
 
 <script src="js/modernizr-2.0.6.min.js"></script>
 
